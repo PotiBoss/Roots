50c6f50d29e4f62e0f73057a0ca96eb0aa3007bb
DESKTOP-P4D6QTM\Mikolaj <potigimboss@gmail.com>
2023-02-03 16:55:50 +0100

    Init

---
+++ Source/Roots/RootsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RootsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOTS_API ARootsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
